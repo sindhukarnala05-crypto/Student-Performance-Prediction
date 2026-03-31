b34ef550e86f08180e397ec4df38ba20b6f04c31
sindhukarnala05-crypto <sindhukarnala05@gmail.com>
2026-03-31 10:17:02 +0530

    Fixed prediction mapping and error

--- app.py
+++ app.py
@@ -14,27 +14,14 @@ studytime = st.slider("Study Time (1-4)", 1, 4)
 failures = st.slider("Failures (0-4)", 0, 4)
 absences = st.slider("Absences", 0, 50)
 
-# Predict button
 if st.button("Predict"):
-
-    # create 30 feature list
     features = [0]*30
-
-    # put user inputs in first positions
     features[0] = studytime
     features[1] = failures
     features[2] = absences
-
     input_data = np.array([features])
-
-    # scale
     input_scaled = scaler.transform(input_data)
-
-    # prediction
     prediction = model.predict(input_scaled)
-    prediction = model.predict(input_scaled)
-
-# Map numeric output to labels
-labels = ["Low", "Medium", "High"]
-result = labels[prediction[0]]
-st.success(f" Predicted Performance: {result}")
+    labels = ["Low", "Medium", "High"]
+    result = labels[prediction[0]]
+    st.success(f"Predicted Performance: {result}")
